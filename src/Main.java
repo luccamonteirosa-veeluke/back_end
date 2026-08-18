9dcb93eb860e59e0338af4eca4c912f0f95ef24f
luccamonteirosa-veeluke <lucca.monteiro.sa@gmail.com>
2026-08-17 22:18:31 -0300

    [feat]: added a class(Cofrinho) and a savings system.

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,22 @@ void main() {
         String op= readln("1-adicionar dinheiro; 2-remover dinheiro ; 3-monstrar saldo; 4-mostrar progresso; 5-mostrar status;6-sair ");
         switch (op){
             case "1":
-            float dinheiro=Float.parseFloat(readln("insira valor para depositar"));
-            c.add_dinheiro(dinheiro);
-
+                float dinheiro=Float.parseFloat(readln("insira valor para depositar"));
+                c.add_dinheiro(dinheiro);
+                continue;
             case "2":
                 float rdinheiro=Float.parseFloat(readln("insira valor para retirar"));
                 c.ret_dinheiro(rdinheiro);
-
+                continue;
             case "3":
                 c.getsaldo();
+                continue;
             case "4":
                 c.progresso();
+                continue;
             case "5":
                 c.getStatus();
+                continue;
             case "6":
                 println("voce saiu do sistema");
                 ex = false;
